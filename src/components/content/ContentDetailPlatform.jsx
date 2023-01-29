8076f48adcae18e4b33dd8fae7246a7f7df195c5
jennajeh <jennajuneh@gmail.com>
2023-01-29 22:35:10 +0900

    플랫폼 사이트 이동 url 설정

--- src/components/content/ContentDetailPlatform.jsx
+++ src/components/content/ContentDetailPlatform.jsx
@@ -11,6 +11,10 @@ export default function ContentDetailPlatform() {
 
   const platformList = platform?.split(',');
 
+  const handleClickPlatform = (url) => {
+    window.open(`http://www.${url}.com`, '_blank');
+  };
+
   return (
     <div>
       <h3 style={{ color: 'red' }}>여기서 감상할 수 있어요</h3>
@@ -18,25 +22,25 @@ export default function ContentDetailPlatform() {
       <div>
         <ul>
           <li>
-            <a href="">
+            <button type="button" onClick={() => handleClickPlatform(platformList?.[0])}>
               {platformList?.[0]}
               {' '}
               바로가기
-            </a>
+            </button>
           </li>
           <li>
-            <a href="">
+            <button type="button" onClick={() => handleClickPlatform(platformList?.[1])}>
               {platformList?.[1]}
               {' '}
               바로가기
-            </a>
+            </button>
           </li>
           <li>
-            <a href="">
+            <button type="button" onClick={() => handleClickPlatform(platformList?.[2])}>
               {platformList?.[2]}
               {' '}
               바로가기
-            </a>
+            </button>
           </li>
         </ul>
       </div>
