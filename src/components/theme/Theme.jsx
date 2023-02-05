f5b5f62769ea4271d23fad49dc0693513e0ee44f
jennajeh <jennajuneh@gmail.com>
2023-02-05 13:55:42 +0900

    테마 추천 페이지 완성

---
+++ src/components/theme/Theme.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const ImgWrapper = styled.article`
+  margin: 1em 0 1em 0;
+
+h4 {
+  text-align: center;
+  font-size: ${((props) => props.theme.size.h6)};
+  color: ${((props) => props.theme.text.primary)};
+}
+
+  img {
+    border-radius: 0.6em;
+    width: 100%;
+  }
+`;
+
+export default function Theme({ theme }) {
+  const { id: themeId, title, image } = theme;
+
+  return (
+    <div>
+      <Link to={`/themes/${themeId}`}>
+        <ImgWrapper>
+          <img src={image} alt="img" />
+          <h4>{title}</h4>
+        </ImgWrapper>
+      </Link>
+    </div>
+  );
+}
--- src/components/theme/ThemeList.jsx
+++ src/components/theme/ThemeList.jsx
@@ -1,10 +1,53 @@
+import styled from 'styled-components';
 import useThemeStore from '../../hooks/useThemeStore';
+import Theme from './Theme';
+
+const SubTitle = styled.h3`
+  font-size: ${((props) => props.theme.size.h5)};
+  font-weight: bold;
+  margin-top: 1em;
+`;
+
+const List = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  gap: 1em;
+`;
+
+const Error = styled.p`
+  margin: 80px;
+  font-weight: 700;
+  font-size: ${((props) => props.theme.size.h4)};
+  text-align: center;
+`;
 
 export default function ThemeList() {
   const themeStore = useThemeStore();
 
   const { themes } = themeStore;
+
+  if (!themes) {
+    return (
+      <p>Loading...</p>
+    );
+  }
+
   return (
-    <div>ThemeList</div>
+    <div>
+      <SubTitle>테마 리스트</SubTitle>
+      {themes.length ? (
+        <List>
+          {themes.map((theme) => (
+            <Theme
+              key={theme.id}
+              theme={theme}
+            />
+          ))}
+        </List>
+      ) : (
+        <Error>작품이 존재하지 않습니다.</Error>
+      )}
+    </div>
   );
 }
