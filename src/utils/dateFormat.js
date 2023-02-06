d9a3910afdb2e7fbd4a70daf7fbface1c717ae44
jennajeh <jennajuneh@gmail.com>
2023-02-06 16:50:31 +0900

    dateFormat 변경

--- src/utils/dateFormat.js
+++ src/utils/dateFormat.js
@@ -1,3 +1,17 @@
 export default function dateFormat(date) {
+  const today = new Date();
+  const timeValue = new Date(date);
+
+  const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+  if (betweenTime < 1) return '방금 전';
+  if (betweenTime < 60) {
+    return `${betweenTime}분 전`;
+  }
+
+  const betweenTimeHour = Math.floor(betweenTime / 60);
+  if (betweenTimeHour < 24) {
+    return `${betweenTimeHour}시간 전`;
+  }
+
   return date.split('T')[0];
 }
